15c72687cc28c3cfafac6424d59b79595e4a422b
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-28 18:34:07 +0300

    Given an integer array, in-place shuffle it. The algorithm should produce an unbiased permutation, i.e., every permutation is equally likely.

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,30 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         int[]  X = {-10, -3, 5, 6, -2};
         solution(X);
     }
-    static void solution(int[] X){
-        int m = X.length;
-        int max=0;
-        for (int i = 0; i < m; i++)
-        {
-             for(int j=i+1;j<m-1;j++) {
-                 if(X[i]*X[j]>max) {
-                    max=X[i]*X[j];
-                 }
-             }
-
-        }
-        System.out.println(max);
-
-//        for(int k=0;k<m;k++){
-//            System.out.print(X[k]);
-//            if(k!=m-1) {
-//                System.out.print(",");
-//            }
-//        }
-//        System.out.println();
-//        for(int z=0;z<n;z++){
-//            System.out.print(Y[z]);
-//            if(z!=n-1) {
-//                System.out.print(",");
-//            }
-//        }
+    private static void swap(int[] A, int i, int j)
+    {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
-}
+
+    // Function to shuffle an array `A[]`
+    public static void solution(int[] A)
+    {
+        int len= A.length;
+        for (int i = len - 1; i >= 1; i--)
+        {
+            Random rand = new Random();
+
+            int j = rand.nextInt(i + 1);
+
+            swap(A, i, j);
+        }
+        System.out.println(Arrays.toString(A));
+    }
+    }
+
