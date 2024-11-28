e8f15fa1ce8abef3c71cff783e5bcd707966f605
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-28 20:06:14 +0300

    Given an integer array, move all zeros present in it to the end. The solution should maintain the relative order of items in the array and should not use constant space.

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,34 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[]  X = {0, -3, 5, -4, -2, 3, 1, 0};
+        int[] X = {6, 0, 8, 2, 3, 0, 4, 0, 1};
 
         solution(X);
     }
-//    private static void swap(int[] A, int i, int j)
-//    {
-//        int temp = A[i];
-//        A[i] = A[j];
-//        A[j] = temp;
-//    }
 
+    private static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
+    }
 
     public static void solution(int[] A) {
         int len = A.length;
+        int index = len - 1;  // Start from the end of the array
+
         for (int i = 0; i < len; i++) {
-            int left = i;
-            int sumLeft = 0;
-            while (left > 0) {
-                sumLeft += A[left - 1];
-                left--;
+            if (A[i] == 0 && A[index] != 0) {
+                swap(A, i, index);  // Swap non-zero element with zero
+                index--;  // Move the index backward to place the next zero
             }
-            int right = i;
-            int sumRight = 0;
-            while (right < len-1) {
-                sumRight += A[right + 1];
-                right++;
-
+            if(A[i] == 0&&i+1<len&&A[i+1]!=0){
+                swap(A, i, i+1);
             }
-            if(sumLeft==sumRight){
-                System.out.println("Equilibrium found at "+ i);
+            else if (A[i] != 0) {
+                continue;
             }
-
         }
+
+        System.out.println(Arrays.toString(A));
     }
 }
