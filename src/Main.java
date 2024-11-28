2787e535d497f98050557829d0f23b81183c6010
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-28 06:32:39 +0300

    Given two sorted arrays, X[] and Y[] of size m and n each, merge elements of X[] with elements of array Y[] by maintaining the sorted order, i.e., fill X[] with the first m smallest elements and fill Y[] with remaining elements.

--- src/Main.java
+++ src/Main.java
@@ -5,38 +5,60 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr= { 0, 0, 1, 0, 1, 0, 0 };
-        solution(arr);
+        int[] x= { 1, 4, 7, 8, 10 };
+        int[] y= { 2, 3, 9 };
+        solution(x,y);
     }
-    static void solution(int[] arr){
-        int len=arr.length;
-        int begin=0;
-        int end=0;
+    static void solution(int[] X, int[] Y){
+        int m = X.length;
+        int n = Y.length;
 
-        for(int i=0;i<len;i++){
-            int m=i;
-            int zero=0;
-            int one=0;
-          while(m<len){
-              if(arr[m]==0){
-                  zero++;
-              }else{
-                  one++;
-              }
-              if(zero==one && (m-i)>(end-begin)){
-                  end=m;
-                  begin=i;
-              }
-              m++;
-          }
-        }
-        for(int k= begin;k<=end;k++){
-            System.out.print(arr[k]);
-            if(k!=end) {
-                System.out.print(",");
+        // Consider each element `X[i]` of array `X` and ignore the element if it is
+        // already in the correct order; otherwise, swap it with the next smaller
+        // element, which happens to be the first element of `Y`.
+        for (int i = 0; i < m; i++)
+        {
+            // compare the current element of `X[]` with the first element of `Y[]`
+            if (X[i] > Y[0])
+            {
+                // swap `X[i]` with `Y[0]`
+                int temp = X[i];
+                X[i] = Y[0];
+                Y[0] = temp;
+
+                int first = Y[0];
+
+                // move `Y[0]` to its correct position to maintain the sorted
+                // order of `Y[]`. Note: `Y[1…n-1]` is already sorted
+                int k;
+                for (k = 1; k < n && Y[k] < first; k++) {
+                    Y[k - 1] = Y[k];
+                }
+
+                Y[k - 1] = first;
             }
         }
 
 
+
+
+
+
+
+
+
+        for(int k=0;k<m;k++){
+            System.out.print(X[k]);
+            if(k!=m-1) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for(int z=0;z<n;z++){
+            System.out.print(Y[z]);
+            if(z!=n-1) {
+                System.out.print(",");
+            }
+        }
     }
 }
