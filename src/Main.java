55e73cbce04b6a25e60d5305108077bc74752f5a
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-28 20:26:49 +0300

    Replace every array element with the product of every other element without using a division operator

--- src/Main.java
+++ src/Main.java
@@ -2,34 +2,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] X = {6, 0, 8, 2, 3, 0, 4, 0, 1};
+        int[] X = { 1, 2, 3, 4, 5 };
 
         solution(X);
     }
 
-    private static void swap(int[] A, int i, int j) {
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
-    }
+//    private static void swap(int[] A, int i, int j) {
+//        int temp = A[i];
+//        A[i] = A[j];
+//        A[j] = temp;
+//    }
 
     public static void solution(int[] A) {
         int len = A.length;
-        int index = len - 1;  // Start from the end of the array
+        int[] result = new int[len];
 
         for (int i = 0; i < len; i++) {
-            if (A[i] == 0 && A[index] != 0) {
-                swap(A, i, index);  // Swap non-zero element with zero
-                index--;  // Move the index backward to place the next zero
-            }
-            if(A[i] == 0&&i+1<len&&A[i+1]!=0){
-                swap(A, i, i+1);
-            }
-            else if (A[i] != 0) {
-                continue;
-            }
+            int product=1;
+           for(int j=0;j<len;j++){
+               if(j==i){
+                   continue;
+               }
+               product*=A[j];
+           }
+            result[i]=product;
+
         }
 
-        System.out.println(Arrays.toString(A));
+        System.out.println(Arrays.toString(result));
     }
 }
