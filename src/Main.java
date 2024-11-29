2640abaa69b9c2fd3e5b72850e507c0ac8db694c
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-29 06:43:45 +0300

    Find the maximum difference between two array elements that satisfies the given constraints

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] X = { 3, 5, 8, 4, 5, 9, 10, 8, 5, 3, 4 };
-
+        int[] X = { 2, 7, 9, 5, 1, 3, 5 };
         solution(X);
     }
     public static void solution(int[] A) {
         int len = A.length;
         int max=0;
-        int begin=0;
-        int end=0;
+        int fist=0;
+        int second=0;
+
         for (int i = 0; i < len; i++) {
-            int k=i;
-            int countAsc=0;
-            int countDesc=0;
+         for(int j=i+1;j<len-1;j++){
+             if(A[j]-A[i]>max){
+                 max=A[j]-A[i];
+                 fist=i;
+                 second=j;
+             }
+         }
 
-          while(k<len-1&&A[k]<A[k+1]){
-              countAsc++;
-              k++;
-          } if(k+1<len&&A[k]>A[k+1]){
-                while(k<len-1&&A[k]>A[k+1]){
-                    countDesc++;
-                    k++;
-                }
-            }
-          if(countAsc+countDesc>max){
-             begin=i;
-             end=k;
-              max=countAsc+countDesc;
-          }
+
 
         }
+        System.out.println(A[fist]+" and the second element is: "+ A[second]+" This is the difference>:"+ max);
 
-        for(int j=begin;j<=end;j++){
-            System.out.print(A[j]);
-            if(j!=end){
-                System.out.print(",");
-            }
-
-        }
     }
 }
