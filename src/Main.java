29bb90dfa675ab89bf145085a59a7473b672918c
cheruiyotcollins <kelvincollins86@gmail.com>
2024-11-29 12:20:09 +0300

    Given a binary array, find the maximum sequence of continuous 1’s that can be formed by replacing at most k zeros by ones.

--- src/Main.java
+++ src/Main.java
@@ -2,33 +2,47 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        int[] X = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        solution(X);
+        int[] X = {1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0};
+        int k = 1;
+        solution(X, k);
     }
-    public static void solution(int[] A) {
-        int len = A.length;
-        int max=0;
-        int firstIndex=0;
-        int lastIndex=0;
 
+    public static void solution(int[] A, int n) {
+        int len = A.length;
+        int max = 0;
         for (int i = 0; i < len; i++) {
-            int sum=0;
-         for(int j=i;j<len-1;j++){
-             sum+=A[j];
-             if(sum>max){
-                 max=sum;
-                 firstIndex=i;
-                 lastIndex=j;
-             }
-         }
-        }
-        System.out.println("This is the Sum:"+ max);
-        for(int j=firstIndex;j<=lastIndex;j++){
-            System.out.print(A[j]);
-            if(j!=lastIndex){
-                System.out.print(",");
+            int count = 0;
+            for (int j = i; j < len; j++) {
+                if (A[j] == 1) {
+                    count++;
+
+                } else {
+                    if (n == 0) {
+                        break;
+                    } else {
+                        int k = n;
+                        int m = j;
+                        while (k > 0 && m < len) {
+                            if (A[m] == 0) {
+                                count++;
+                                k--;
+                                m++;
+                            } else if (A[m] == 1) {
+                                count++;
+                                m++;
+                            }
+
+
+                        }
+                    }
+                }
+                if (count > max) {
+                    max = count;
+                }
             }
+
         }
+        System.out.println(max);
 
     }
 }
