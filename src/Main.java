b216ea52123b62336c66768c8e73f729abb52165
cheruiyotcollins <kelvincollins86@gmail.com>
2024-12-01 16:25:00 +0300

    final

--- src/Main.java
+++ src/Main.java
@@ -17,68 +17,58 @@ public class Main {
     }
 
     public static int slotWheels(List<String> history) {
-        List<List<Integer>> intHistory = stringIntegerConverter(history);
-        List<int[]> subHistory = new ArrayList<>();
-        List<Integer> totalStopsArr = new ArrayList<>();
-        Set<Integer> setInt = new HashSet<>();
-        int numWheels = intHistory.getFirst().size();
-        int totalStops = 0;
-        int maxStops = 0;
-        int len = intHistory.size();
-        for (int i = 0; i < len; i++) {
-            maxStops = 0;
-
-            for (List<Integer> spin : intHistory) {
-                for (int col = 0; col < spin.size(); col++) {
-                    if (spin.get(col) > maxStops) {
-                        maxStops = spin.get(col);
-                    }
-                }
-            }
-            for (List<Integer> spin : intHistory) {
-                int maxRow = 0;
-                int indexMax = -1;
-                int temp=0;
-                for (int col = 0; col < spin.size(); col++) {
-
-                    if (spin.get(col) == maxStops && spin.get(col) != 0) {
-                        setInt.add(maxStops); // Add the global maximum to the set
-                        spin.remove(col);
-                        if(spin.size()==1){
-                             temp= spin.getFirst();
-                        }
-                        break;
-                    }
-                    if (spin.get(col) > maxRow &&spin.get(col) != maxStops ) {
-                        maxRow = spin.get(col);
-                        indexMax = col;
-
-                    }
-                }
-                if (indexMax != -1 && maxRow != maxStops) {
-                    spin.remove(indexMax);
-                }
-                if(temp>0){
-                    spin.add(temp);
+        List<List<Integer>> intHistory= stringIntegerConverter(history);
+        Set<Integer> setInt= new HashSet<>();
+        int totalStops=0;
+        int maxStops=0;
+        for(List<Integer> spin: intHistory){
+            for(int col=0;col<spin.size();col++){
+                if(spin.get(col)>maxStops){
+                    maxStops=spin.get(col);
                 }
             }
         }
-        System.out.println(setInt);
-        for (int j : setInt) {
-            totalStops += j;
+        for(List<Integer> spin: intHistory){
+            int maxRow=0;
+            int indexMax=-1;
+            int temp=0;
+            for(int col=0;col<spin.size();col++){
+                if(spin.get(col)==maxStops&&spin.get(col)!=0){
+                    setInt.add(maxStops);
+                    spin.remove(col);
+                    if(spin.size()==1){
+                        temp=spin.get(0);
+                    }
+                    break;
+                }
+                if(spin.get(col)>maxRow&&spin.get(col)!=maxStops){
+                    maxRow=spin.get(col);
+                    indexMax=col;
+                }
+            }
+            if(indexMax!=-1&&maxRow!=maxStops){
+                spin.remove(indexMax);
+            }
+            if(temp>0){
+                spin.add(temp);
+            }
+        }
 
+
+        for(int j: setInt){
+            totalStops+=j;
         }
         return totalStops;
+
     }
-
-    public static List<List<Integer>> stringIntegerConverter(List<String> history) {
-        List<List<Integer>> integerList = new ArrayList<>();
-
-        for (String s : history) {
-            String[] parts = s.split(",");
+    public static List<List<Integer>> stringIntegerConverter(List<String> history){
+        List<List<Integer>> integerList= new ArrayList<>();
+        for(String s: history){
+            String[] parts =s.split(",");
             List<Integer> integers = new ArrayList<>();
-            for (String part : parts) {
+            for(String part:parts){
                 integers.add(Integer.parseInt(part));
+
             }
             integerList.add(integers);
         }
