0a553761ecf82187a4cb7e344f0813a8cbc8cc85
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-10 07:38:27 +0300

    Given a string str, the task is to reverse the order of the words in the given string. Note that str may contain leading or trailing dots(.) or multiple trailing dots(.) between two words. The returned string should only have a single dot(.) separating the words.

--- src/Main.java
+++ src/Main.java
@@ -4,44 +4,30 @@ import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        int[][] intervals = {
-                {1, 3},
-                {4, 5},
-                {6, 7},
-                {8, 10}
-        };
-        int[] newInterval={5,6};
-
-        ArrayList<int[]> intervalOutput = insertInterval(intervals,newInterval);
-        for (int[] interval : intervalOutput) {
-            System.out.println(Arrays.toString(interval));
-        }
+        String str = "..geeks..for.geeks.";
+        System.out.println(stringReversal(str));
     }
 
-    public static ArrayList<int[]> insertInterval(int[][] arr, int[] newInterval) {
-        ArrayList<int[]> res = new ArrayList<>();
-        Collections.addAll(res, arr);
-
-        int i = 0;
-        int len = res.size();
-
-        // Add intervals before newInterval
-        while (i < len && res.get(i)[1] < newInterval[0]) {
-            i++;
+    public static String stringReversal(String str) {
+        String[] strArray= str.split("\\.");
+        int len=strArray.length;
+        String[] reversedStr=new String[len];
+        int j=len-1;
+        for (String s : strArray) {
+            reversedStr[j] = s;
+            j--;
+        }
+        StringBuilder stringBuilder= new StringBuilder();
+        for(int i=0;i<len;i++){
+            if(!reversedStr[i].isEmpty()){
+            stringBuilder.append(reversedStr[i]);
+            }
+            if(i!=len-1&&!reversedStr[i].isEmpty()){
+                stringBuilder.append(".");
+            }
         }
 
-        // Merge overlapping intervals
-        while (i < len && res.get(i)[0] <= newInterval[1]) {
-            newInterval[0] = Math.min(newInterval[0], res.get(i)[0]);
-            newInterval[1] = Math.max(newInterval[1], res.get(i)[1]);
-            res.remove(i);
-            len--;
-        }
-
-        // Insert the merged interval
-        res.add(i, newInterval);
-
-        return res;
+        return stringBuilder.toString();
     }
 //    public static List<List<Integer>> stringIntegerConverter(List<String> history){
 //        List<List<Integer>> integerList= new ArrayList<>();
