dd1e8e3fc4a050419043892c70bafe1b562504d2
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-25 07:30:58 +0300

    In Java, substring() method of String class returns a substring from the given string. This method is most useful when you deal with text manipulation, parsing, or data extraction.

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,35 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "..geeks..for.geeks.";
-        System.out.println(stringReversal(str));
+        String str = "geeksforgeeks";
+        System.out.println(longestSubstring(str));
     }
 
-    public static String stringReversal(String str) {
-        String[] strArray= str.split("\\.");
-        int len=strArray.length;
-        String[] reversedStr=new String[len];
-        int j=len-1;
-        for (String s : strArray) {
-            reversedStr[j] = s;
-            j--;
-        }
-        StringBuilder stringBuilder= new StringBuilder();
-        for(int i=0;i<len;i++){
-            if(!reversedStr[i].isEmpty()){
-            stringBuilder.append(reversedStr[i]);
-            }
-            if(i!=len-1&&!reversedStr[i].isEmpty()){
-                stringBuilder.append(".");
-            }
+    public static String longestSubstring(String str) {
+        int start=0;
+        int stop=0;
+        int max=0;
+        int len=str.length();
+        for (int i=0; i<len;i++) {
+           int k=i;
+            Set<Character> uniqueCharacters=new HashSet<>();
+           while(k<len){
+
+               if(uniqueCharacters.contains(str.charAt(k))){
+                   if(k-i>max){
+                       max=k-i;
+                       start=k;
+                       stop=i;
+                   }
+               }
+               uniqueCharacters.add(str.charAt(k));
+               k++;
+           }
         }
 
-        return stringBuilder.toString();
+
+        return str.substring(start,stop);
     }
 //    public static List<List<Integer>> stringIntegerConverter(List<String> history){
 //        List<List<Integer>> integerList= new ArrayList<>();
