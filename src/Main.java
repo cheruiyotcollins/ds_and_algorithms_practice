f4958c88a3766b48165ae088a6f9a34688f7535f
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-25 21:11:52 +0300

    Given two strings S (length m) and P (length n), the task is to find the smallest substring in S that contains all characters of P, including duplicates. If no such substring exists, return “-1”. If multiple substrings of the same length are found, return the one with the smallest starting index.

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,39 @@
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        String str = "geeekk";
-        System.out.println(longestSubstring(str));
+        String str = "timetopractice";
+        String inp="toc";
+
+        System.out.println(containsAllCharacters(str,inp));
     }
 
 
-    public static char longestSubstring(String str) {
+    public static char containsAllCharacters(String str,String inp) {
         char mostOccuring = 0;
-        int maxLength = 0;
+        int maxLength = Integer.MAX_VALUE;
+        int start=0;
+        int end=0;
         int len= str.length();
         for (int i= 0; i< str.length(); i++) {
+            String temp=inp;
         char currentChar = str.charAt(i);
-        int current=i;
-
-            while (current<len&&str.charAt(current)==currentChar) {
-                current++;
+         int k=i;
+            while (k<len&&!temp.isEmpty()) {
+                if(temp.contains(String.valueOf(str.charAt(k))) ){
+                    int index = temp.indexOf(str.charAt(k));
+                    temp = temp.substring(index + 1);
+                }
+                k++;
             }
 
-            if (current - i > maxLength) {
-                maxLength = current-i;
-                mostOccuring = currentChar;
+            if (k - i < maxLength) {
+                maxLength =k-i;
+                start = i;
+                end=k;
             }
         }
 
-        System.out.println(maxLength);
+        System.out.println(start);
+        System.out.println(end);
         return mostOccuring;
     }
 
