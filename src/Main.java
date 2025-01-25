fcd89cc279a24af043e0a23a18141b4e563dfaab
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-25 19:42:07 +0300

    Given a set of non-overlapping intervals and a new interval, the task is to insert the interval at the correct position such that after insertion, the intervals remain sorted. If the insertion results in overlapping intervals, then merge the overlapping intervals. Assume that the set of non-overlapping intervals is sorted based on start time. bug fix

--- src/Main.java
+++ src/Main.java
@@ -6,31 +6,36 @@ public class Main {
         System.out.println(longestSubstring(str));
     }
 
-    public static String longestSubstring(String str) {
-        int start=0;
-        int stop=0;
-        int max=0;
-        int len=str.length();
-        for (int i=0; i<len;i++) {
-           int k=i;
-            Set<Character> uniqueCharacters=new HashSet<>();
-           while(k<len){
 
-               if(uniqueCharacters.contains(str.charAt(k))){
-                   if(k-i>max){
-                       max=k-i;
-                       start=k;
-                       stop=i;
-                   }
-               }
-               uniqueCharacters.add(str.charAt(k));
-               k++;
-           }
+    public static String longestSubstring(String str) {
+        int start = 0; // Start of the current window
+        int maxStart = 0; // Start index of the longest substring
+        int maxLength = 0; // Length of the longest substring
+        Set<Character> uniqueCharacters = new HashSet<>();
+
+        for (int end = 0; end < str.length(); end++) {
+            char currentChar = str.charAt(end);
+
+            // If the character is already in the set, shrink the window from the start
+            while (uniqueCharacters.contains(currentChar)) {
+                uniqueCharacters.remove(str.charAt(start));
+                start++;
+            }
+
+            // Add the current character to the set
+            uniqueCharacters.add(currentChar);
+
+            // Update maxLength and maxStart if the current window is the largest so far
+            if (end - start + 1 > maxLength) {
+                maxLength = end - start + 1;
+                maxStart = start;
+            }
         }
 
-
-        return str.substring(start,stop);
+        // Return the longest substring
+        return str.substring(maxStart, maxStart + maxLength);
     }
+
 //    public static List<List<Integer>> stringIntegerConverter(List<String> history){
 //        List<List<Integer>> integerList= new ArrayList<>();
 //        for(String s: history){
