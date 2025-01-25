050e38d0e87e2caa54e62520f37e3e2fd99d3f85
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-25 21:28:41 +0300

    Given two strings S (length m) and P (length n), the task is to find the smallest substring in S that contains all characters of P, including duplicates. If no such substring exists, return “-1”. If multiple substrings of the same length are found, return the one with the smallest starting index. bug fix

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         String str = "timetopractice";
@@ -7,35 +9,61 @@ public class Main {
     }
 
 
-    public static char containsAllCharacters(String str,String inp) {
-        char mostOccuring = 0;
-        int maxLength = Integer.MAX_VALUE;
-        int start=0;
-        int end=0;
-        int len= str.length();
-        for (int i= 0; i< str.length(); i++) {
-            String temp=inp;
-        char currentChar = str.charAt(i);
-         int k=i;
-            while (k<len&&!temp.isEmpty()) {
-                if(temp.contains(String.valueOf(str.charAt(k))) ){
-                    int index = temp.indexOf(str.charAt(k));
-                    temp = temp.substring(index + 1);
-                }
-                k++;
-            }
 
-            if (k - i < maxLength) {
-                maxLength =k-i;
-                start = i;
-                end=k;
-            }
+
+    public static String containsAllCharacters(String S, String P) {
+        // Frequency map for characters in P
+        Map<Character, Integer> freqMap = new HashMap<>();
+        for (char c : P.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
         }
 
-        System.out.println(start);
-        System.out.println(end);
-        return mostOccuring;
+        int required = freqMap.size(); // Number of unique characters in P
+        int formed = 0; // Number of characters in the current window that match P's frequency
+
+        Map<Character, Integer> windowCounts = new HashMap<>();
+        int left = 0, right = 0;
+        int minLength = Integer.MAX_VALUE;
+        int startIndex = -1;
+
+        // Sliding window
+        while (right < S.length()) {
+            // Add the current character to the window
+            char c = S.charAt(right);
+            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
+
+            // Check if the character matches the frequency in P
+            if (freqMap.containsKey(c) && windowCounts.get(c).equals(freqMap.get(c))) {
+                formed++;
+            }
+
+            // Try to shrink the window from the left
+            while (formed == required) {
+                // Update the smallest substring
+                if (right - left + 1 < minLength) {
+                    minLength = right - left + 1;
+                    startIndex = left;
+                }
+
+                // Remove the left character from the window
+                char leftChar = S.charAt(left);
+                windowCounts.put(leftChar, windowCounts.get(leftChar) - 1);
+
+                // If removing this character breaks the condition, decrement formed
+                if (freqMap.containsKey(leftChar) && windowCounts.get(leftChar) < freqMap.get(leftChar)) {
+                    formed--;
+                }
+
+                left++; // Shrink the window
+            }
+
+            right++; // Expand the window
+        }
+
+        // Return the result
+        return startIndex == -1 ? "-1" : S.substring(startIndex, startIndex + minLength);
     }
+
 
 //    public static List<List<Integer>> stringIntegerConverter(List<String> history){
 //        List<List<Integer>> integerList= new ArrayList<>();
