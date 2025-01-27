39e589ad67e0fdc528bf91a9b49f6c3783e3ec10
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-27 09:14:57 +0300

    Given two strings s1 and s2 consisting of lowercase characters, the task is to check whether the two given strings are anagrams of each other or not. An anagram of a string is another string that contains the same characters, only the order of characters can be different.

--- src/Main.java
+++ src/Main.java
@@ -2,67 +2,30 @@ import java.util.HashMap;
 import java.util.Map;
 public class Main {
     public static void main(String[] args) {
-        String str = "timetopractice";
-        String inp="toc";
-
-        System.out.println(containsAllCharacters(str,inp));
+        String s1 = "geeks";
+        String s2="skeeg";
+        System.out.println(findAnagram(s1,s2));
     }
 
 
+    public static boolean findAnagram(String s1, String s2) {
+        boolean isAnagram=true;
+        int len= s1.length();
+        int j=len-1;
+       for(int i=0;i<len;i++){
+           if(s1.charAt(i)!=s2.charAt(j)){
+               System.out.println(s1.charAt(i));
+               System.out.println(s1.charAt(j));
+               isAnagram=false;
+               break;
+           }
+           j--;
+       }
 
 
-    public static String containsAllCharacters(String S, String P) {
-        // Frequency map for characters in P
-        Map<Character, Integer> freqMap = new HashMap<>();
-        for (char c : P.toCharArray()) {
-            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
-        }
-
-        int required = freqMap.size(); // Number of unique characters in P
-        int formed = 0; // Number of characters in the current window that match P's frequency
-
-        Map<Character, Integer> windowCounts = new HashMap<>();
-        int left = 0, right = 0;
-        int minLength = Integer.MAX_VALUE;
-        int startIndex = -1;
-
-        // Sliding window
-        while (right < S.length()) {
-            // Add the current character to the window
-            char c = S.charAt(right);
-            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
-
-            // Check if the character matches the frequency in P
-            if (freqMap.containsKey(c) && windowCounts.get(c).equals(freqMap.get(c))) {
-                formed++;
-            }
-
-            // Try to shrink the window from the left
-            while (formed == required) {
-                // Update the smallest substring
-                if (right - left + 1 < minLength) {
-                    minLength = right - left + 1;
-                    startIndex = left;
-                }
-
-                // Remove the left character from the window
-                char leftChar = S.charAt(left);
-                windowCounts.put(leftChar, windowCounts.get(leftChar) - 1);
-
-                // If removing this character breaks the condition, decrement formed
-                if (freqMap.containsKey(leftChar) && windowCounts.get(leftChar) < freqMap.get(leftChar)) {
-                    formed--;
-                }
-
-                left++; // Shrink the window
-            }
-
-            right++; // Expand the window
-        }
-
-        // Return the result
-        return startIndex == -1 ? "-1" : S.substring(startIndex, startIndex + minLength);
+        return  isAnagram;
     }
+
 
 
 //    public static List<List<Integer>> stringIntegerConverter(List<String> history){
