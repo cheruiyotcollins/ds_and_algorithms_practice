e4bd3b43068df1f8e5c3f9b9103e852d671d5c08
cheruiyotcollins <kelvincollins86@gmail.com>
2025-01-27 23:47:47 +0300

    Given a string str, the task is to find the longest substring which is a palindrome. If there are multiple answers, then return the first appearing substring.

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,36 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        String arr = "Too hot to hoot.";
+        String arr = "forgeeksskeegfor";
         System.out.println(isSentencePalindrome(arr));
     }
 
     public static String isSentencePalindrome(String s1) {
         boolean isPalindrome=true;
-        String s1Arr= s1.replaceAll("[^a-zA-Z0-9]","").toLowerCase(Locale.ROOT);
+        int largest=0;
+        int start=0;
+        int end =0;
 
-        int len= s1Arr.length()-1;
+        int len= s1.length()-1;
         for(int i=0;i<=len;i++){
-            if(s1Arr.charAt(i)!=s1Arr.charAt(len)){
-                isPalindrome=false;
-                break;
+            if(s1.charAt(i)!=s1.charAt(len)){
+                len--;
+                continue;
             }
-            len--;
+            int k=i;
+            int m=len;
+            while(s1.charAt(i)==s1.charAt(len)){
+                i++;
+                len--;
+            }
+            if(m-k>largest){
+                largest=m-k;
+               start=k;
+               end=m;
+            }
+
         }
-        return isPalindrome? "Is Palindrome": "Not Palindrome";
+        return s1.substring(start,end+1);
     }
 }
 
